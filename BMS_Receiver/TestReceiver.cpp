f291fe8b388f11bdc609bda9f9917d67ce0ab0fb
RaveendranArun <arunr4957@gmail.com>
2021-06-22 00:10:40 +0530

    test code

--- BMS_Receiver/TestReceiver.cpp
+++ BMS_Receiver/TestReceiver.cpp
@@ -4,11 +4,11 @@
 #include <cmath>
 
 string dummy_data[6] = {"{\"temperature\" : 92.11,\"soc\" : 62.55,\"chargingrate\" : 0.4 }",
-						"{\"Temperature\" : -36.23,\"SOC\" : 8.38,\"chargingrate\" : 0.1}", 
-						"{\"Temperature\" : 28.43,\"SOC\" : 5.82,\"chargingrate\" : 0.5}", 
-						"{\"Temperature\" : 46.25,\"SOC\" : 21.13,\"chargingrate\" : 0.8}", 
-						"{\"Temperature\" : 52.10,\"SOC\" : 34.08,\"chargingrate\" : 1.0}", 
-						"{\"Temperature\" : -24.01,\"SOC\" : 84.86,\"chargingrate\" : 0.08}"
+			"{\"temperature\" : -36.23,\"soc\" : 8.38,\"chargingrate\" : 0.1}", 
+			"{\"temperature\" : 28.43,\"soc\" : 5.82,\"chargingrate\" : 0.5}", 
+			"{\"temperature\" : 46.25,\"soc\" : 21.13,\"chargingrate\" : 0.8}", 
+			"{\"temperature\" : 52.10,\"soc\" : 34.08,\"chargingrate\" : 1.0}", 
+			"{\"temperature\" : -24.01,\"soc\" : 84.86,\"chargingrate\" : 0.08}"
                        };
 
 bool compare_float(float x, float y, float epsilon = 0.01f) {
@@ -23,11 +23,11 @@ TEST_CASE("validate sender data")
     BmsParamters params;
 	
     for (int i = 0; i < 6; i++)
-	{
-		REQUIRE(receiver.ParseDataFromConsole(dummy_data[i], params));
-		auto& list = receiver.GetBmsDataContainer();
-		list.push_back(params);		
-	}
+    {
+        REQUIRE(receiver.ParseDataFromConsole(dummy_data[i], params));
+        auto& list = receiver.GetBmsDataContainer();
+	list.push_back(params);		
+    }
 
 	//validate list size
 	REQUIRE(receiver.GetBmsDataContainer().size() == 6);
@@ -38,19 +38,19 @@ TEST_CASE("validate sender data")
 	//positive test cases - validate paramters with expected output after calculation
 	REQUIRE(compare_float(stat.minTemperature , -36.23));
 	REQUIRE(compare_float(stat.maxTemperature , 92.11));
-	REQUIRE(compare_float(stat.minSoc , 26));
-	REQUIRE(compare_float(stat.maxSoc , 66));
+	REQUIRE(compare_float(stat.minSoc , 5.82));
+	REQUIRE(compare_float(stat.maxSoc , 84.86));
 	REQUIRE(compare_float(stat.minChargeRate , 0.08));
 	REQUIRE(compare_float(stat.maxChargeRate, 1.0));
-    REQUIRE(compare_float(stat.avgTemperature, 13.308));
-    REQUIRE(compare_float(stat.avgSoc, 30.854));
-    REQUIRE(compare_float(stat.avgChargeRate, 0.496));
+    	REQUIRE(compare_float(stat.avgTemperature, 13.308));
+    	REQUIRE(compare_float(stat.avgSoc, 30.854));
+    	REQUIRE(compare_float(stat.avgChargeRate, 0.496));
 
 	//Negative test by injecting inappropriate json format
-    string data = "aa bb cc";
+    	string data = "aa bb cc";
 	REQUIRE(!receiver.ParseDataFromConsole(data, params));
-    data = "{\"Temperature:-32,\"SOC\" : 36}";
+    	data = "{\"Temperature:-32,\"SOC\" : 36}";
 	REQUIRE(!receiver.ParseDataFromConsole(data, params));
-    data = "{\"Temperature:one,\"SOC\" : two}";
+    	data = "{\"Temperature:one,\"SOC\" : two}";
 	REQUIRE(!receiver.ParseDataFromConsole(data, params));
 }
